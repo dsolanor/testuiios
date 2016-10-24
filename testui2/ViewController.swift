dff25a1364c0e4d5f7d4065764a7ce78eda369cd
David Solano <davidsolano@MacBook-Pro-de-David.local>
2016-10-24 18:55:35 +0200

    Initial Commit

---
+++ testui2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testui2
+//
+//  Created by David Solano on 24/10/16.
+//  Copyright © 2016 David Solano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
